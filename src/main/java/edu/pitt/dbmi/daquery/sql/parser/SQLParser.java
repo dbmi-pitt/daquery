d78262236654bea792209dffaaeb260fe8b2ecbb
shirey <bill.shirey@gmail.com>
2017-06-20 16:06:11 -0400

    removed unneeded import from SQLParser

--- src/main/java/edu/pitt/dbmi/daquery/sql/parser/SQLParser.java
+++ src/main/java/edu/pitt/dbmi/daquery/sql/parser/SQLParser.java
@@ -2,8 +2,6 @@ package edu.pitt.dbmi.daquery.sql.parser;
 
 import java.util.List;
 
-import edu.pitt.dbmi.daquery.dev.SQLPrintLListener;
-import edu.pitt.dbmi.daquery.sql.domain.SelectQuery;
 
 
 public class SQLParser
