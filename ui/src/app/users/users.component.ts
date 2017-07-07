7c90ba19ee7254dddac514316abd41527b7d10b9
etlds <del20@pitt.edu>
2017-07-07 11:08:06 -0400

    import ui code

---
+++ ui/src/app/users/users.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../shared/user.service';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+  showAddUser = false;
+
+  users: any[] = [];
+  constructor(private userService: UserService) {
+    this.users = this.userService.getUsers();
+  }
+
+  ngOnInit() {
+  }
+
+}
