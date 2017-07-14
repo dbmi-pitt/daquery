6605ce15d313f0491583f98ff4e2343b3491e9ee
etlds <del20@pitt.edu>
2017-07-14 14:05:46 -0400

    edit route.ts

--- ui/src/routes.ts
+++ ui/src/routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from "@angular/router"
-import { QueriesToMeComponent } from "./app/queries-to-me/queries-to-me.component"
-import { QueriesFromMeComponent } from "./app/queries-from-me/queries-from-me.component"
-import { SitesComponent } from "./app/sites/sites.component"
-import { UsersComponent } from "./app/users/users.component"
-import { ReviewQueryComponent } from "./app/queries-to-me/review-query/review-query.component"
+import { QueriesToMeComponent } from "./app/components/queries-to-me/queries-to-me.component"
+import { QueriesFromMeComponent } from "./app/components/queries-from-me/queries-from-me.component"
+import { SitesComponent } from "./app/components/sites/sites.component"
+import { UsersComponent } from "./app/components/users/users.component"
+import { ReviewQueryComponent } from "./app/components/queries-to-me/review-query/review-query.component"
 
 export const appRoutes:Routes = [
     { path: "queries-to-me", component: QueriesToMeComponent },
