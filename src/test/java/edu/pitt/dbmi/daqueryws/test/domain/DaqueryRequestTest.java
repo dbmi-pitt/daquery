cb7f8cae35bc23468f8a31f0b125939772fa3178
cborromeo <cborromeo@pitt.edu>
2017-11-07 11:50:05 -0500

    - created DaqueryRequest test.  Covers incoming requests

--- src/test/java/edu/pitt/dbmi/daqueryws/test/domain/DaqueryRequestTest.java
+++ src/test/java/edu/pitt/dbmi/daqueryws/test/domain/DaqueryRequestTest.java
@@ -1,8 +1,6 @@
 package edu.pitt.dbmi.daqueryws.test.domain;
 
 import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
 
 import org.hibernate.Session;
 import org.junit.AfterClass;
@@ -15,9 +13,7 @@ import edu.pitt.dbmi.daquery.dao.HibernateConfiguration;
 import edu.pitt.dbmi.daquery.dao.NetworkDAO;
 import edu.pitt.dbmi.daquery.dao.SiteDAO;
 import edu.pitt.dbmi.daquery.domain.DaqueryUser;
-import edu.pitt.dbmi.daquery.domain.DataSource;
 import edu.pitt.dbmi.daquery.domain.Network;
-import edu.pitt.dbmi.daquery.domain.SQLDataSource;
 import edu.pitt.dbmi.daquery.domain.Site;
 import edu.pitt.dbmi.daquery.domain.inquiry.DaqueryRequest;
 import edu.pitt.dbmi.daquery.domain.inquiry.RequestDirection;
@@ -26,7 +22,9 @@ import edu.pitt.dbmi.daquery.domain.inquiry.RequestDirection;
 public class DaqueryRequestTest {
 
 	private static String networkname = "DaqueryRequestTest_Network";
+	private static String networkUUID = "";
 	private static String inrequestname = "In Request Name";
+	private static String inrequestUUID = "";
 	private static String outrequestname = "Out Request Name";
 	private static String connectionurl = "jdbc:oracle:thin:@//server-a.dept.university.edu:1521/OracleSID";
 	private static String username = "datasourceusername";
@@ -39,6 +37,7 @@ public class DaqueryRequestTest {
 	private static String accesskey = "TestSiteAccessKey";
 	private static String commenckey = "TestSiteCommEncKey";
 	private static String commenctype = "TestSitecommEncType";
+	private static String siteUUID = "";
 	
 	//DaqueryUser data elements
 	private static String daqueryusername = "DaqueryRequestTest_User";
@@ -50,16 +49,20 @@ public class DaqueryRequestTest {
 	public static void setupBeforeClass() {
     	AppProperties.setDevHomeDir("/opt/apache-tomcat-6.0.53");
 		//create a new network for testing
+    	//NOTE: you must create the site in this method to ensure the site is 
+    	//created before the tests are run
 		Session session = null;
 		try {
 	    	session = HibernateConfiguration.openSession();
-			Network n = new Network();
+			Network n = new Network(true);
 			n.setName(networkname);			
 			session.getTransaction().begin();
 			session.persist(n);
 			session.getTransaction().commit();
 			
-			Site s = new Site();
+			networkUUID = n.getNetworkId();
+			
+			Site s = new Site(true);
 			s.setName(sitename);
 			s.setAdmin_email(adminemail);
 			s.setNetwork(n);
@@ -67,6 +70,8 @@ public class DaqueryRequestTest {
 			session.persist(s);
 			session.update(n);
 			session.getTransaction().commit();
+			
+			siteUUID = s.getSiteId();
 			
 			DaqueryUser u = new DaqueryUser();
 			u.setUsername(username);
@@ -93,25 +98,24 @@ public class DaqueryRequestTest {
 		Session session = null;
 		try {
 	    	session = HibernateConfiguration.openSession();
-	    	Network n = NetworkDAO.queryNetworkByName(networkname);
-	    	Site s = SiteDAO.querySiteByName(sitename);
+	    	Network n = NetworkDAO.queryNetwork(networkUUID);
+	    	Site s = SiteDAO.querySiteByID(siteUUID);
 	    	DaqueryUser u = DaqueryUserDAO.queryUserByUsername(daqueryusername);
-	    	DaqueryRequest dr = new DaqueryRequest();
+	    	DaqueryRequest dr = new DaqueryRequest(true);
 			dr.setRequestId(inrequestname);
 			dr.setDirectionEnum(RequestDirection.IN);
 			dr.setRequester(u);
 			dr.setRequestSite(s);
+			dr.setNetwork(n);
 			Date timestamp = new Date();
 			dr.setSentTimestamp(timestamp);
 			
-			Set<DaqueryRequest> newRequests = new HashSet<DaqueryRequest>();
-			newRequests.add(dr);
-			n.setIncomingRequests(newRequests);
-			
 			session.getTransaction().begin();
+			session.update(s);
 			session.persist(dr);
-			session.update(n);
 			session.getTransaction().commit();
+			inrequestUUID = dr.getRequestId();
+			
 		} catch (Exception e) {
 			e.printStackTrace(System.out);
 		} finally {
@@ -127,17 +131,17 @@ public class DaqueryRequestTest {
 		try {
 	    	session = HibernateConfiguration.openSession();
 			session.getTransaction().begin();
-			session.createQuery("delete from Site where name = :sitename")
-				.setParameter("sitename", sitename)
+			session.createQuery("delete from Site where siteId = :siteUUID")
+				.setParameter("siteUUID", siteUUID)
 				.executeUpdate();
-			session.createQuery("delete from Network where name = :networkname")
-				.setParameter("networkname", networkname)
+			session.createQuery("delete from Network where networkId = :networkUUID")
+				.setParameter("networkUUID", networkUUID)
 				.executeUpdate();
 			session.createQuery("delete from DaqueryUser where username = :username")
 				.setParameter("username", username)
 				.executeUpdate();
-			session.createQuery("delete from DaqueryRequest where id = :requestid")
-				.setParameter("requestid", inrequestname)
+			session.createQuery("delete from DaqueryRequest where requestId = :requestid")
+				.setParameter("requestid", inrequestUUID)
 				.executeUpdate();
 			session.getTransaction().commit();
 		} catch (Exception e) {
