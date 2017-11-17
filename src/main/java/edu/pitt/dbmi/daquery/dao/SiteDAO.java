3871901125ed6553459d30502fc4ab5334585abb
Desheng Li <del20@pitt.edu>
2017-11-17 11:54:04 -0500

    get outgoing sites

--- src/main/java/edu/pitt/dbmi/daquery/dao/SiteDAO.java
+++ src/main/java/edu/pitt/dbmi/daquery/dao/SiteDAO.java
@@ -8,7 +8,10 @@ import java.util.List;
 import java.util.logging.Logger;
 
 import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
 
+import edu.pitt.dbmi.daquery.domain.Network;
 import edu.pitt.dbmi.daquery.domain.Site;
 import edu.pitt.dbmi.daquery.dao.ParameterItem;
 
@@ -75,7 +78,33 @@ public class SiteDAO extends AbstractDAO {
     		logger.info(e.getLocalizedMessage());
             throw e;
         }
-    }	
+    }
+    
+    /** Get sites by network_id
+     *  @param network_id
+     *  @return List<Site>
+     * @throws Exception 
+     */
+    public static List<Site> queryConnectedOutgoingSitesByNetworkId(long network_id) throws Exception{
+    	Session s = null;
+    	try {
+    		s = HibernateConfiguration.openSession();
+			
+			String sql = "SELECT s.* FROM SITE as s JOIN OUTGOING_QUERY_SITES as oqs ON s.id = oqs.site_id JOIN NETWORK as n ON n.id = oqs.network_id WHERE s.status='CONNECTED' and n.id = :network_id";
+			Query query = s.createSQLQuery(sql)
+						   .addEntity(Site.class)
+						   .setParameter("network_id", network_id);
+			
+			List result = query.list();
+    		
+	        return result;
+	    
+        } catch (HibernateException e) {
+    		logger.info("Error unable to connect to database.  Please check database settings.");
+    		logger.info(e.getLocalizedMessage());
+            throw e;
+        }
+    }
 
 }
 
