e742ec4d69b4cc7eaece2a7a9ac36e6e6878e226
Bill Shirey <shirey@pitt.edu>
2017-11-30 10:14:59 -0500

    reworked initial db setup

--- src/dev/java/edu/pitt/dbmi/daquery/dev/TestAppSetup.java
+++ src/dev/java/edu/pitt/dbmi/daquery/dev/TestAppSetup.java
@@ -1,25 +1,39 @@
 package edu.pitt.dbmi.daquery.dev;
 
 import edu.pitt.dbmi.daquery.common.util.AppSetup;
+import edu.pitt.dbmi.daquery.common.util.DaqueryException;
+
+import java.util.UUID;
+
 import edu.pitt.dbmi.daquery.common.util.AppProperties;
 
 public class TestAppSetup
 {
-	public static void main(String [] args)
+	public static void main(String [] args) throws DaqueryException
 	{
-		//AppProperties.setDevHomeDir("/opt/apache-tomcat-6.0.53");
-		AppProperties.setDevHomeDir("/home/devuser/dq-data");
-		AppSetup.initialSetup("path@pitt.edu", "password", null, null, null, null);
-		if(AppSetup.isErroredSetup())
-			System.err.println(AppSetup.getErrorMessage());
-		else if(AppSetup.isValidSetup())
-		{
-/*			if(AppSetup.wasFirstUserCreated())
-				System.out.println(AppSetup.getFirstUserDetails()); */			
-			System.out.println("All Good");
-		}
-		else
-			System.out.println("Invalid setup, but no error reported");
+		TestAppSetup.setupDB("/opt/apache-tomcat-6.0.53", "dqadmin@pitt.edu", "temppassword", "dq-test-site", "http://localhost:8008/", "Test User");
 	}
-	
+
+	/**
+	 * Create an initial database for a local site.  The following parameters are required.
+	 * 
+	 * @param dbFilePath The local file system path to the location where the database will
+	 *                   be stored. e.g. for a db stored in the Tomcat directory "/opt/apache-tomcat-6.0.53"
+	 * @param adminEmail The email address of the admin for the site
+	 * @param adminPassword The password for the admin account
+	 * @param siteName The name of the site being managed from this database 
+	 * @param localSiteURL The URL where this site can be reached by other sites
+	 * @return
+	 */
+	public static void setupDB(String dbFilePath, String adminEmail, String adminPassword, String siteName, String localSiteURL, String adminRealName) throws DaqueryException
+	{
+		AppProperties.setDevHomeDir(dbFilePath);
+		AppSetup.initialSetup(UUID.randomUUID().toString(), siteName, localSiteURL, adminEmail, adminPassword, adminRealName);
+		if(AppSetup.isErroredSetup())
+			throw new DaqueryException(AppSetup.getErrorMessage());
+		else if(!AppSetup.isValidSetup())
+			throw new DaqueryException("Invalid DB setup, but no error was reported.");
+		
+		
+	}
 }
