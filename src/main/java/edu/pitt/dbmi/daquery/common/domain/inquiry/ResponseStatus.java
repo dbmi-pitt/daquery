4666d40eba634f3d0973d88e2738d20dc3f5d397
Bill Shirey <shirey@pitt.edu>
2018-01-18 08:37:41 -0500

    chaged response status type from abandoned to stalled

--- src/main/java/edu/pitt/dbmi/daquery/common/domain/inquiry/ResponseStatus.java
+++ src/main/java/edu/pitt/dbmi/daquery/common/domain/inquiry/ResponseStatus.java
@@ -2,14 +2,13 @@ package edu.pitt.dbmi.daquery.common.domain.inquiry;
 
 public enum ResponseStatus
 {
-	APPROVED(TYPES.APPROVED),
 	DENIED(TYPES.DENIED),
 	PENDING(TYPES.PENDING),
 	QUEUED(TYPES.QUEUED),
 	EXECUTING(TYPES.EXECUTING),
 	COMPLETED(TYPES.COMPLETED),
 	ERROR(TYPES.ERROR),
-	ABANDONED(TYPES.ABANDONED);
+	STALLED(TYPES.STALLED);
 	
 	ResponseStatus(String val)
 	{
@@ -31,13 +30,12 @@ public enum ResponseStatus
 	
 	public static class TYPES
 	{
-		public static final String APPROVED = "APPROVED";
 		public static final String DENIED = "DENIED";
 		public static final String PENDING = "PENDING";
 		public static final String QUEUED = "QUEUED";
 		public static final String EXECUTING = "EXECUTING";
 		public static final String ERROR = "ERROR";
 		public static final String COMPLETED = "COMPLETED";
-		public static final String ABANDONED = "ABANDONED";
+		public static final String STALLED = "STALLED";
 	} 
 }
