2643a431bea8638033e3f34398a75442f4d5f4e8
Bill Shirey <shirey@pitt.edu>
2018-03-05 11:30:05 -0500

    removed logger from DataExport so it will compile

--- src/main/java/edu/pitt/dbmi/daquery/util/properties/DataExport.java
+++ src/main/java/edu/pitt/dbmi/daquery/util/properties/DataExport.java
@@ -1,26 +1,15 @@
 package edu.pitt.dbmi.daquery.util.properties;
+import java.io.StringReader;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
-import javax.xml.bind.annotation.*;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-import java.util.logging.Level;
-import edu.pitt.dbmi.daquery.common.util.AppProperties;
-import edu.pitt.dbmi.daquery.rest.RequestEndpoint;
-
-import java.io.FileNotFoundException;
-import java.io.StringReader;
-import java.util.*;
-import java.util.logging.Logger;
 
 
 public class DataExport {
 	public DataExportConfig dataModel = new DataExportConfig();
 	
-	private final static Logger logger = Logger.getLogger(DataExport.class.getName());
+	//private final static Logger logger = Logger.getLogger(DataExport.class.getName());
 	
 	public DataExport(String dataExportConf) throws PropfileException {
 		try {
