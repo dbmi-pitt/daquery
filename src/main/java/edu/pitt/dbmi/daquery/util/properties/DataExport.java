aab88d58aecf0fc6a22f2886637451217673548b
Desheng Li <del20@pitt.edu>
2018-03-05 11:35:34 -0500

    rename dataExportConfig

--- src/main/java/edu/pitt/dbmi/daquery/util/properties/DataExport.java
+++ src/main/java/edu/pitt/dbmi/daquery/util/properties/DataExport.java
@@ -7,7 +7,7 @@ import javax.xml.bind.Unmarshaller;
 
 
 public class DataExport {
-	public DataExportConfig dataModel = new DataExportConfig();
+	public DataExportConfig dataExportConfig = new DataExportConfig();
 	
 	//private final static Logger logger = Logger.getLogger(DataExport.class.getName());
 	
@@ -16,7 +16,7 @@ public class DataExport {
 			JAXBContext jaxbContext = JAXBContext.newInstance(DataExportConfig.class);
 			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
 			StringReader reader = new StringReader(dataExportConf);
-			this.dataModel = (DataExportConfig) jaxbUnmarshaller.unmarshal(reader);
+			this.dataExportConfig = (DataExportConfig) jaxbUnmarshaller.unmarshal(reader);
 		} catch(JAXBException je) {
 			throw new PropfileException("Error reading XML property string.  Possibly the XML is not formated correctly.", PropfileException.XML_ERROR);
 		}
