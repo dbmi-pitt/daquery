d6f254e690de74c625cceb34e9f040074418e7cb
bill.shirey@gmail.com <bill.shirey@gmail.com>
2018-03-07 21:59:37 -0500

    added ability to change max run length for a given named task queue

--- src/main/java/edu/pitt/dbmi/daquery/queue/QueueManager.java
+++ src/main/java/edu/pitt/dbmi/daquery/queue/QueueManager.java
@@ -4,6 +4,8 @@ import java.util.Hashtable;
 
 import org.hibernate.internal.util.StringHelper;
 
+import edu.pitt.dbmi.daquery.common.util.DaqueryException;
+
 public class QueueManager
 {
 	private static Hashtable<String, TaskQueue> queues = new Hashtable<String, TaskQueue>(); 
@@ -12,11 +14,38 @@ public class QueueManager
 		if(StringHelper.isEmpty(queueName))
 			return(null);
 
-		String key = queueName.toLowerCase().trim();
+		String key = keyValue(queueName);
 		if(!queues.containsKey(key))
 			queues.put(key, new TaskQueue());
 
 		return(queues.get(key));
 	}
+
+	public synchronized static void addQueue(String queueName, int maxRunLength) throws DaqueryException
+	{
+		if(StringHelper.isEmpty(queueName))
+			throw new DaqueryException("Queue name must not be blank.");
+		
+		String key = keyValue(queueName);
+		
+		if(queues.containsKey(key))
+			throw new DaqueryException("Task queue with name " + queueName + " already exists.");
 	
+		if(maxRunLength <= 0)
+			throw new DaqueryException("Task queue max run size must be greter than zero.");
+		
+		queues.put(key, new TaskQueue(maxRunLength));
+	}
+
+	public synchronized static boolean containsQueue(String queueName)
+	{
+		if(StringHelper.isEmpty(queueName)) return(false);
+		return(queues.containsKey(keyValue(queueName)));
+	}
+	private static String keyValue(String val)
+	{
+		if(val == null) return(null);
+		
+		return(val.toLowerCase().trim());
+	}
 }
