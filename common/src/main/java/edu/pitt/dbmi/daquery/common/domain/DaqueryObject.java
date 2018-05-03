dfac3b66aeda59cf06b1ba2a856c97959d91db82
etlds <del20@pitt.edu>
2018-05-03 10:16:54 -0400

    remove unEscapeQuotes

--- common/src/main/java/edu/pitt/dbmi/daquery/common/domain/DaqueryObject.java
+++ common/src/main/java/edu/pitt/dbmi/daquery/common/domain/DaqueryObject.java
@@ -46,6 +46,7 @@ public abstract class DaqueryObject implements Serializable{
                         //return new JsonPrimitive(ISODateTimeFormat.dateTime().print(src));
                     }
                 }).create();
-		return StringHelper.unEscapeQuotes(gson.toJson(this));
+		//return StringHelper.unEscapeQuotes(gson.toJson(this));
+        return gson.toJson(this);
 	}
 }
