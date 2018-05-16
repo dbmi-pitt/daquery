d0d87c1f5e053e970bd86167454cd76804dca2d7
shirey <bill.shirey@gmail.com>
2018-05-16 08:51:03 -0400

    change batch file to put build.version in resource directory

--- ws/git.info.bat
+++ ws/git.info.bat
@@ -1,10 +1,10 @@
 @Echo off
-del /q build.version
+del /q src\main\resources\build.version
 git rev-parse --abbrev-ref HEAD > branch.temp
 git shortlog | find /c /v "~`!@#$%^&*()_+" > commits.temp
 set /p BR=<branch.temp
 set /p CM=<commits.temp
-echo branch=%BR% > build.version
-echo build.no=%CM% >> build.version
+echo branch=%BR% > src\main\resources\build.version
+echo build.no=%CM% >> src\main\resources\build.version
 del /q branch.temp
 del /q commits.temp
