0cf130feffa1414eebf97abefc19eea6cddabb28
Bill Shirey <shirey@pitt.edu>
2018-05-16 08:53:13 -0400

    change script to put build.version in resource directory

--- ws/git.info.sh
+++ ws/git.info.sh
@@ -1,3 +1,3 @@
-rm -f build.version
-echo branch=`git rev-parse --abbrev-ref HEAD` > build.version
-echo build.no=`git shortlog | wc -l` >> build.version
+rm -f src/main/resources/build.version
+echo branch=`git rev-parse --abbrev-ref HEAD` > src/main/resources/build.version
+echo build.no=`git shortlog | wc -l` >> src/main/resources/build.version
