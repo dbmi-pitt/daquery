0ce089b64b419951c977f0a0ac8063d739e516f6
Bill Shirey <shirey@pitt.edu>
2018-06-19 07:59:30 -0400

    set c3p0 properties on datasource connection not application db

--- common/src/main/java/edu/pitt/dbmi/daquery/common/util/C3P0Properties.java
+++ common/src/main/java/edu/pitt/dbmi/daquery/common/util/C3P0Properties.java
@@ -7,11 +7,14 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class C3P0Properties
 {
@@ -20,12 +23,46 @@ public class C3P0Properties
 	private static File propFile = null;
 	private static Properties props = null;
 
+	public static void setProperties(ComboPooledDataSource cpds)
+	{
+		try
+		{
+			for(String name : getPropertyNames())
+			{
+				System.out.println(name);
+				if(StringHelper.startsWithIgnoreCase(name, "c3p0."))
+				{
+					String value = null;
+					String propertyName = name.substring(5);
+					try
+					{
+						value = C3P0Properties.getProperty(name);
+						ReflectionHelper.setValue(cpds, ComboPooledDataSource.class, propertyName, value);
+					}
+					catch (InvocationTargetException | IllegalAccessException | IllegalArgumentException | DaqueryException e)
+					{
+						log.log(Level.SEVERE, "Unable to set c3p0 property " + name + " with value " + value, e);
+					}
+				}
+			}
+		}
+		catch(DaqueryException e)
+		{
+			log.log(Level.SEVERE, "Error while accessiong c3p0 properties file.  c3p0 properties not set.", e);
+		}		
+	}
+	
 	public static String getProperty(String name) throws DaqueryException
 	{
 		return getProperties().getProperty(name, "");
 	}
 	
-	private static Properties getProperties() throws DaqueryException
+	public static boolean fileExists() throws DaqueryException
+	{
+		File f = getPropertyFile();
+		return(f != null && f.exists());
+	}
+	public static Properties getProperties() throws DaqueryException
 	{
 		if(props == null)
 		{
@@ -38,7 +75,7 @@ public class C3P0Properties
 			catch(DaqueryException de){throw de;}
 			catch(FileNotFoundException fnfe){throw new DaqueryException("Could not find jdbc/hibernate/3p0 properties file.", fnfe);}
 			catch(Throwable t){
-				String msg = "Unexpected error while acquiring jdbc/hibernate/c3po properties file";
+				String msg = "Unexpected error while acquiring c3po properties file";
 				log.log(Level.SEVERE, msg, t);
 				throw new DaqueryException(msg, t);
 			}
@@ -71,31 +108,27 @@ public class C3P0Properties
 				}
 			}
 			catch(DaqueryException ex){throw ex;}
-			catch(IOException ioex){throw new DaqueryException("Error while reading from jdbc/hibernate property file.", ioex);}
-			catch(Throwable t){throw new DaqueryException("Unexpected Error while reading from jdbc/hibernate property file.", t);}
+			catch(IOException ioex){throw new DaqueryException("Error while reading from c3p0 properties file.", ioex);}
+			catch(Throwable t){throw new DaqueryException("Unexpected Error while reading from c3p0 properties file.", t);}
 			finally
 			{
 				if(reader != null)
 				{
 					try{reader.close();}
-					catch(IOException iox){log.log(Level.WARNING, "Error while trying to close jdbc/hibernate property file. ", iox);}
+					catch(IOException iox){log.log(Level.WARNING, "Error while trying to close c3p0 propertis file. ", iox);}
 				}
 			}
 		}
 		return(propertyNames);
 	}
-	
+
 	private static File getPropertyFile() throws DaqueryException
 	{
 		if(propFile == null)
 		{
 			String confDir = AppProperties.getConfDirectory();
-			String filePath = confDir + "jdbc.connection.properties";
+			String filePath = confDir + "daquery.c3p0.properties";
 			propFile = new File(filePath);
-			if(! (propFile.exists() && propFile.canRead()))
-			{
-				throw new DaqueryException("Cannot read jdbc properties file at " + filePath);
-			}
 		}
 		return(propFile);
 	}
