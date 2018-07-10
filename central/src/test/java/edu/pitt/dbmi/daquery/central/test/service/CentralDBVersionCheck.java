2e10d3a3c83ca88e22429b639645fafd2c4be718
Bill Shirey <shirey@pitt.edu>
2018-07-10 08:59:14 -0400

    added junit tests to check db version in DDL vs max update version

---
+++ central/src/test/java/edu/pitt/dbmi/daquery/central/test/service/CentralDBVersionCheck.java
@@ -0,0 +1,43 @@
+package edu.pitt.dbmi.daquery.central.test.service;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import edu.pitt.dbmi.daquery.common.util.AppProperties;
+import edu.pitt.dbmi.daquery.common.util.DDLReader;
+import edu.pitt.dbmi.daquery.common.util.DaqueryException;
+import edu.pitt.dbmi.daquery.update.UpdateDBForVersion;
+
+public class CentralDBVersionCheck {
+
+	private static final float INFINITESIMAL = 1e-10f;
+	@Test
+	public void test()
+	{
+		try
+		{
+			String ddlFile = AppProperties.getInitializationDDL();
+			Float ddlVersion = DDLReader.getVerion(ddlFile);
+			float updateVersion = UpdateDBForVersion.getMaxUpdateVersion();
+			float upMin = updateVersion - INFINITESIMAL;
+			float upMax = updateVersion + INFINITESIMAL;
+			if(ddlVersion == null)
+				fail("No db version found for central db in " + ddlFile);
+			if(! (upMin < ddlVersion.floatValue() && upMax > ddlVersion.floatValue()))
+				fail("Central db ddl version and maximum update version do not match");
+		}
+		catch(DaqueryException e)
+		{
+			e.printStackTrace(System.err);
+			fail(e.getMessage());
+		}
+		catch(IOException iox)
+		{
+			iox.printStackTrace(System.err);			
+			fail("IOException while looking for max update version.");
+		}
+	}
+}
