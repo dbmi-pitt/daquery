9bebbcd2da30b546d7f7f914f7bb202efc2f184d
cborromeo <chb69@pitt.edu>
2018-08-28 09:11:08 -0400

    updated the command line parameters to accept JDBC driver full path (including .jar file)

--- docker/docker-baseline/start_site.sh
+++ docker/docker-baseline/start_site.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo 'Deploy JDBC driver'
-cp $CONTAINER_OJDBC_DIR/ojdbc6-11.1.0.7.0.jar $TOMCAT_HOME/lib
+cp $CONTAINER_OJDBC_DIR $TOMCAT_HOME/lib
 
 echo 'Deploy daquery.war to Tomcat'
 cp $CONTAINER_DAQUERY_WAR_DIR/daquery.war $TOMCAT_HOME/webapps/
--- docker/scripts/all-start-test.sh
+++ docker/scripts/all-start-test.sh
@@ -5,7 +5,7 @@
 
 
 ./start-test-central.sh --daquery_home=/home/devuser/projects/daquery --db_home=/home/devuser/daquery_docker_data
-./start-test-connected.sh --daquery_home=/home/devuser/projects/daquery --db_home=/home/devuser/daquery_docker_data --ojdbc_lib=/home/devuser/projects/daquery/ws/lib
-./start-test-noconnection.sh --daquery_home=/home/devuser/projects/daquery --db_home=/home/devuser/daquery_docker_data --ojdbc_lib=/home/devuser/projects/daquery/ws/lib
+./start-test-connected.sh --daquery_home=/home/devuser/projects/daquery --db_home=/home/devuser/daquery_docker_data --ojdbc_lib=/home/devuser/projects/daquery/ws/lib/ojdbc6-11.1.0.7.0.jar
+./start-test-noconnection.sh --daquery_home=/home/devuser/projects/daquery --db_home=/home/devuser/daquery_docker_data --ojdbc_lib=/home/devuser/projects/daquery/ws/lib/ojdbc6-11.1.0.7.0.jar
 
 
