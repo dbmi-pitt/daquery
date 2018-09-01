206143e025a6150effe2da4ba917401e746f6298
bill.shirey@gmail.com <bill.shirey@gmail.com>
2018-09-01 14:00:31 -0400

    pass in docker setup vars to start-docker.sh with environment variables

---
+++ docker/scripts/set-vars-bill-mac.sh
@@ -0,0 +1,4 @@
+export DAQUERY_HOME="/Users/bill/Documents/workspace/daquery"
+export DB_HOME="/Users/bill/daquery_docker_data"
+export OJDBC_LIB="/Users/bill/Documents/workspace/daquery/ws/lib/ojdbc7.jar"
+export DAQUERY_CENT_URL="http://10.195.22.66:8080"
