1ecfab2687d55a02c8c8fcac8b1921bc610b5971
Bill Shirey <shirey@pitt.edu>
2018-09-04 14:33:47 -0400

    fixed issue with too many inquiries being returned

--- common/src/main/java/edu/pitt/dbmi/daquery/common/dao/InquiryDAO.java
+++ common/src/main/java/edu/pitt/dbmi/daquery/common/dao/InquiryDAO.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
+import org.hibernate.Query;
 import org.hibernate.criterion.Restrictions;
 
 import edu.pitt.dbmi.daquery.common.domain.inquiry.Inquiry;
@@ -33,10 +34,10 @@ public class InquiryDAO extends AbstractDAO {
 											 .uniqueResult();
 	}
 	
-	public List list() throws DaqueryException{
+/*	public List list() throws DaqueryException{
 		return getCurrentSession().createCriteria(Inquiry.class)
 				  				  .list();
-	}
+	} */
 	
 	public void update(long id, Inquiry query) throws DaqueryException {
 		getCurrentSession().update(query);
@@ -47,16 +48,32 @@ public class InquiryDAO extends AbstractDAO {
 	}
 	
 	public List listSavedInquiries() throws DaqueryException {
-		List i =  getCurrentSession().createCriteria(Inquiry.class)
-				  				     .list();
+		Query q = getCurrentSession().createQuery("select i from Inquiry i");
+		List i = q.list();
+		//List i =  getCurrentSession().createCriteria(Inquiry.class).list();
 		List ret = new ArrayList();
+//		long inquiryCount = 0;
+//		long yCount = 0;
+//		long returnedYCount = 0;
 		for(Object a : i) {
+//			inquiryCount++;
 			Inquiry inquiry = ((Inquiry) a);
+//			if(inquiry.getInquiryName() != null && inquiry.getInquiryName().equals("Y"))
+//			{
+//				yCount++;
+//				System.out.println("here");
+//			}
 			if(inquiry.getRequests().size() == 0)
 				if(inquiry.getDataType().equals("SQL_QUERY")) 
+				{
 					ret.add((SQLQuery)inquiry);
+//					if(inquiry.getInquiryName() != null && inquiry.getInquiryName().equals("Y"))
+//					{
+//						returnedYCount++;
+//					}
+				}
 		}
-		
+//		System.out.println(inquiryCount + ":" + yCount + ":" + returnedYCount);
 		return ret;
 	}
 }
