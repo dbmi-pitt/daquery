b15d8ab2f2b80754986d3472b7cc5c13743a24a7
cborromeo <chb69@pitt.edu>
2018-09-05 09:29:30 -0400

    fixed some paths

--- docker/scripts/test/all-start-test-jenkins.sh
+++ docker/scripts/test/all-start-test-jenkins.sh
@@ -4,8 +4,11 @@
 # These items can be used for testing.
 
 
+echo 'Running: scripts/test/start-test-central.sh --daquery_home=/home/jenkins/projects/daquery --db_home=/home/jenkins/daquery_docker_data'
 scripts/test/start-test-central.sh --daquery_home=/home/jenkins/projects/daquery --db_home=/home/jenkins/daquery_docker_data
+echo 'Running: scripts/test/start-test-connected.sh --daquery_home=/home/jenkins/projects/daquery --db_home=/home/jenkins/daquery_docker_data --ojdbc_lib_dir=/home/jenkins/daquery_lib/ --central_url="http://136.142.228.23:4001"'
 scripts/test/start-test-connected.sh --daquery_home=/home/jenkins/projects/daquery --db_home=/home/jenkins/daquery_docker_data --ojdbc_lib_dir=/home/jenkins/daquery_lib/ --central_url="http://136.142.228.23:4001"
+echo 'Running: scripts/test/start-test-noconnection.sh --daquery_home=/home/jenkins/projects/daquery --db_home=/home/jenkins/daquery_docker_data --ojdbc_lib_dir=/home/jenkins/daquery_lib/ --central_url="http://136.142.228.23:4001"'
 scripts/test/start-test-noconnection.sh --daquery_home=/home/jenkins/projects/daquery --db_home=/home/jenkins/daquery_docker_data --ojdbc_lib_dir=/home/jenkins/daquery_lib/ --central_url="http://136.142.228.23:4001"
 
 
