92f633c3e9b6de6d512853c8661d2ce1bdfc2602
chb69 <chb69@pitt.edu>
2018-09-13 12:15:49 -0400

    downgraded the Angluar CLI version in package.json.  Added the build-all.sh script to the all-start-test.sh.  Added the common and central builds to build-all.sh

--- docker/scripts/test/all-start-test.sh
+++ docker/scripts/test/all-start-test.sh
@@ -3,9 +3,14 @@
 # This file launches 3 items: a Daquery Central Server and two sites.
 # These items can be used for testing.
 
+# build the code before testing it on Docker
+cd ../../..
+./build-all.sh
 
-./start-test-central.sh --daquery_home=/home/devuser/projects/daquery --db_home=/home/devuser/daquery_docker_data
-./start-test-connected.sh --daquery_home=/home/devuser/projects/daquery --db_home=/home/devuser/daquery_docker_data --ojdbc_lib_dir=/home/devuser/projects/daquery/ws/lib/ --central_url="http://10.0.2.15:4001"
-./start-test-noconnection.sh --daquery_home=/home/devuser/projects/daquery --db_home=/home/devuser/daquery_docker_data --ojdbc_lib_dir=/home/devuser/projects/daquery/ws/lib/ --central_url="http://10.0.2.15:4001"
+cd docker/scripts/test
+
+./start-test-central.sh --daquery_home=/Users/chb69/git/daquery --db_home=/Users/chb69/daquery_docker_data
+./start-test-connected.sh --daquery_home=/Users/chb69/git/daquery --db_home=/Users/chb69/daquery_docker_data --ojdbc_lib_dir=/Users/chb69/git/daquery/ws/lib/ --central_url="http://130.49.179.45:4001"
+./start-test-noconnection.sh --daquery_home=/Users/chb69/git/daquery --db_home=/Users/chb69/daquery_docker_data --ojdbc_lib_dir=/Users/chb69/git/daquery/ws/lib/ --central_url="http://130.49.179.45:4001"
 
 
