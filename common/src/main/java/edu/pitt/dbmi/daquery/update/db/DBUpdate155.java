12dc0c02480a4e8e41aa52e88a49eef2e20adb54
Bill Shirey <shirey@pitt.edu>
2018-09-17 14:48:46 -0400

    increased INQUERY.INQUERY_NAME to 120 chars

---
+++ common/src/main/java/edu/pitt/dbmi/daquery/update/db/DBUpdate155.java
@@ -0,0 +1,22 @@
+package edu.pitt.dbmi.daquery.update.db;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+import edu.pitt.dbmi.daquery.common.domain.SiteStatus;
+import edu.pitt.dbmi.daquery.update.DBUpdater;
+import edu.pitt.dbmi.daquery.update.DBVersion;
+
+@DBVersion(version=1.55f, ddlFile="db-update-v1.55.ddl")
+public class DBUpdate155 implements DBUpdater {
+
+	@Override
+	public void updateData(Connection conn) throws Exception {
+		//nothing to do for this update
+	}
+	
+	@Override
+	public void updateCentralData(Connection conn) throws Exception {
+		//nothing to do on central for this update
+	}
+}
--- common/src/main/resources/daquery.ddl
+++ common/src/main/resources/daquery.ddl
@@ -1,4 +1,4 @@
-<!-- daquery.data.version=1.54 -->
+<!-- daquery.data.version=1.55 -->
 
 CREATE TABLE "APP"."NETWORK"
 (
