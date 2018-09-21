3f9389c4a8aa83716b9d0a87e957797be8dcd8ce
Bill Shirey <shirey@pitt.edu>
2018-09-21 11:28:15 -0400

    removed unneeded docker files

--- docker/docker-bill/start_central.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo 'Deploy daquery-central.war to Tomcat'
-cp $CONTAINER_DAQUERY_WAR_DIR/daquery-central.war $TOMCAT_HOME/webapps/daquery-central.war
-/usr/local/tomcat/bin/catalina.sh run
