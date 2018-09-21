9bc5e7c6d7618e704eb80355d542e999782e9412
Bill Shirey <shirey@pitt.edu>
2018-09-21 10:41:33 -0400

    add Daquery startup options to tomcat startup script

--- docker/docker-baseline/start_site.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo 'Deploy JDBC driver A' > /tmp/ds.log
-cp $CONTAINER_OJDBC_DIR/*.jar $TOMCAT_HOME/lib/ >> /tmp/ds.log 2>&1
-
-echo 'Deploy daquery.war to Tomcat' >> /tmp/ds.log 2>&1
-cp $CONTAINER_DAQUERY_WAR_DIR/daquery.war $TOMCAT_HOME/webapps/ >> /tmp/ds.log 2>&1
-/usr/local/tomcat/bin/catalina.sh run
