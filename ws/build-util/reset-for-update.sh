f077597cfd7c147d22282e652955ac8fe729cb76
Bill Shirey <shirey@pitt.edu>
2018-09-26 22:04:55 -0400

    changed docker baseline to work with auto-update

---
+++ ws/build-util/reset-for-update.sh
@@ -0,0 +1,7 @@
+rm -rf /opt/apache-tomcat-6.0.53/conf/daquery-centraldb
+rm -rf /opt/apache-tomcat-6.0.53/conf/daquery-db
+cp -rp /opt/apache-tomcat-6.0.53/conf/daquery-centraldb.bak /opt/apache-tomcat-6.0.53/conf/daquery-centraldb
+cp -rp /opt/apache-tomcat-6.0.53/conf/daquery-db.bak /opt/apache-tomcat-6.0.53/conf/daquery-db
+rm -rf /home/devuser/daquery_docker_data/daquery-4001
+cp -rp /home/devuser/daquery_docker_data/daquery-4001.bak /home/devuser/daquery_docker_data/daquery-4001
+cp /home/devuser/projects/daquery.war.1529 /home/devuser/projects/daquery/ws/target/daquery.war 
