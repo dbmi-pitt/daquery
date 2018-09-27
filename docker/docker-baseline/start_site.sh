986647450e72983f6fd607ce2c93d2aa7cbd6515
Bill Shirey <shirey@pitt.edu>
2018-09-26 22:04:55 -0400

    changed docker baseline to work with auto-update

--- docker/docker-baseline/start_site.sh
+++ docker/docker-baseline/start_site.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+if [ ! -f $DAQUERY_HOME ]; then
+  mkdir $DAQUERY_HOME
+fi
+if [ ! -f $DAQUERY_CONF_DIR ]; then
+  mkdir $DAQUERY_CONF_DIR ]
+fi
+cp -rfp /usr/local/tomcat/conf/* $DAQUERY_CONF_DIR
+rm -rf /usr/local/tomcat/conf
+ln -s $DAQUERY_CONF_DIR /usr/local/tomcat/conf
+
 /usr/local/tomcat/bin/startup.sh
 /bin/bash
