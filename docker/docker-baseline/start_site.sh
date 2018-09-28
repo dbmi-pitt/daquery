1f39ebd93f91acaa5475e97d8030230015f329ae
Bill Shirey <shirey@pitt.edu>
2018-09-28 15:41:44 -0400

    copy war and jar files at docker startup not tomcat startup

--- docker/docker-baseline/start_site.sh
+++ docker/docker-baseline/start_site.sh
@@ -9,5 +9,10 @@ cp -rfp /usr/local/tomcat/conf/* $DAQUERY_CONF_DIR
 rm -rf /usr/local/tomcat/conf
 ln -s $DAQUERY_CONF_DIR /usr/local/tomcat/conf
 
+echo 'Deploy JDBC driver A' > /tmp/ds.log
+cp $CONTAINER_OJDBC_DIR/*.jar $TOMCAT_HOME/lib/ >> /tmp/ds.log 2>&1
+echo 'Deploy daquery.war to Tomcat' >> /tmp/ds.log 2>&1
+cp $CONTAINER_DAQUERY_WAR_DIR/daquery.war $TOMCAT_HOME/webapps/ >> /tmp/ds.log 2>&1
+
 /usr/local/tomcat/bin/startup.sh
 /bin/bash
