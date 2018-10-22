829babdffa6b57b338e83a94e8494834f36df3b3
cborromeo <chb69@pitt.edu>
2018-10-22 18:21:10 -0400

    removed explicity JAVA_HOME path since it does not exist on Jenkins

--- test/testng.sh
+++ test/testng.sh
@@ -1,4 +1,4 @@
-export JAVA_HOME=/usr/java/jdk1.8.0_171-amd64
+#export JAVA_HOME=/usr/java/jdk1.8.0_171-amd64
 export CLASSPATH=$CLASSPATH:target/test-classes
 mvn -DskipTests clean install
 mvn exec:java -Dexec.mainClass="org.testng.TestNG" -Dexec.args="testng.xml" -Dbrowser="chrome"
