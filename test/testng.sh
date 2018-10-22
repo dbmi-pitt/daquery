20d34b4cf0c441ad8c3176dd9b3cfd85e605b81e
cborromeo <chb69@pitt.edu>
2018-10-22 18:34:17 -0400

    switched to firefox

--- test/testng.sh
+++ test/testng.sh
@@ -1,6 +1,6 @@
 #export JAVA_HOME=/usr/java/jdk1.8.0_171-amd64
 export CLASSPATH=$CLASSPATH:target/test-classes
 mvn -DskipTests clean install
-mvn exec:java -Dexec.mainClass="org.testng.TestNG" -Dexec.args="testng.xml" -Dbrowser="chrome"
+mvn exec:java -Dexec.mainClass="org.testng.TestNG" -Dexec.args="testng.xml" -Dbrowser="firefox"
 
 
