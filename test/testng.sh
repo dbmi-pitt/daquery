fcf12503db18eca7042ad72bd157f44d1078b297
cborromeo <chb69@pitt.edu>
2018-10-24 12:24:37 -0400

    adding some echo statements to the .sh script

--- test/testng.sh
+++ test/testng.sh
@@ -1,4 +1,8 @@
 #export JAVA_HOME=/usr/java/jdk1.8.0_171-amd64
+echo 'IP Config'
+echo | ifconfig
+echo 'Docker Info'
+echo | docker ps
 export CLASSPATH=$CLASSPATH:target/test-classes
 mvn -DskipTests clean install
 mvn exec:java -Dexec.mainClass="org.testng.TestNG" -Dexec.args="testng.xml" -Dbrowser="firefox"
