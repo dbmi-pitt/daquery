28e688dc84ae7115d8caba233f03090ddb733f20
cborromeo <chb69@pitt.edu>
2019-01-16 16:13:34 -0500

    changed test code.  Reset the TokenManager token expiration time to 15 minutes.  Set it to 1 minute in the test code

--- common/src/test/java/edu/pitt/dbmi/daquery/common/test/TokenManagerTest.java
+++ common/src/test/java/edu/pitt/dbmi/daquery/common/test/TokenManagerTest.java
@@ -46,6 +46,7 @@ public class TokenManagerTest {
 		try {
 			for (int i=0; i < 5; i++) {
 				TokenManager tm = TokenManager.getTokenManager();
+				tm.setExpirationMinutes(1);
 				String token = tm.addToken(userUuid, siteUUID, networkUUID);
 				KeyedJWT jwt = tm.getToken(token);
 				validTokens.add(token);
